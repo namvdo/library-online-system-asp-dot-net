f9b7252709064df475c08c52b5fce2fe1af1be39
namvdo <vannam12a7@gmail.com>
2020-11-09 21:05:52 +0700

    Add generic classes

--- library-online-system-asp-dot-net/DAOs/CategoryDAO.cs
+++ library-online-system-asp-dot-net/DAOs/CategoryDAO.cs
@@ -1,7 +1,33 @@
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using library_online_system_asp_dot_net.Models;
+
 namespace library_online_system_asp_dot_net.DAOs
 {
     public class CategoryDAO
     {
+        private static SqlConnection genericConnection;
+
+        static CategoryDAO()
+        {
+            genericConnection = InitConnection.GetInstance().GetConnection();
+        }
+
+        public static List<Category> GetAllCategories()
+        {
+            List<Category> categories = new List<Category>(); 
+            string sql = "select * from Category";
+            SqlCommand cmd = new SqlCommand(sql);
+            cmd.Connection.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+               Category category = new Category((string) reader["category_name"]);
+               categories.Add(category);
+            }
+
+            return categories;
+        }
         
     }
 }
--- library-online-system-asp-dot-net/Models/Category.cs
+++ library-online-system-asp-dot-net/Models/Category.cs
@@ -2,6 +2,17 @@ namespace library_online_system_asp_dot_net.Models
 {
     public class Category
     {
-        
+        private string categoryName;
+
+        public Category(string categoryName)
+        {
+            this.categoryName = categoryName;
+        }
+
+        public string CategoryName
+        {
+            get => categoryName;
+            set => categoryName = value;
+        }
     }
 }
